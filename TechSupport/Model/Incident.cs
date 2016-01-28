770eea013fc99ff3538120e809d0a8cc48aae9fa
Kenji Okamoto <kokamot1@my.westga.edu>
2016-01-28 15:08:36 -0800

    Create class to model an incident

---
+++ TechSupport/Model/Incident.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TechSupport.Model
+{
+    public class Incident
+    {
+        public string ProductCode { get; set; }
+        public DateTime DateOpened { get; set; }
+        public string Customer { get; set; }
+        public string Technician { get; set; }
+        public string Title { get; set; }
+
+    }
+}
