764089233fb5f25100f2dcbdd885ee773169cb84
Kenji Okamoto <kokamot1@my.westga.edu>
2016-01-28 15:09:22 -0800

    Create database connection and query classes

---
+++ TechSupport/DBAccess/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace TechSupport
+{
+    public static class DBConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=TechSupport;" +
+                "Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
---
+++ TechSupport/DBAccess/IncidentData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using TechSupport.Model;
+
+
+namespace TechSupport.DBAccess
+{
+    public static class IncidentData
+    {
+
+        public static List<Incident> GetOpenIncidents()
+        {
+            List<Incident> incidentList = new List<Incident>();
+            SqlConnection connection = DBConnection.GetConnection();
+            string selectStatement =
+                "SELECT i.ProductCode, i.DateOpened, c.Name as Customer, t.Name as Technician, i.Title " +
+                "FROM Incidents i " +
+                "JOIN Customers c ON c.CustomerID = i.CustomerID " +
+                "LEFT OUTER JOIN Technicians t ON t.TechID = i.TechID " +
+                "WHERE i.DateClosed IS NULL" ;
+            SqlCommand selectCommand = new SqlCommand(selectStatement, connection);
+            SqlDataReader reader = null;
+
+            try
+            {
+                connection.Open();
+                reader = selectCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Incident incident = new Incident();
+                    incident.ProductCode = reader["ProductCode"].ToString();
+                    incident.DateOpened = (DateTime)reader["DateOpened"];
+                    incident.Customer = reader["Customer"].ToString();
+                    incident.Technician = reader["Technician"].ToString();
+                    incident.Title = reader["Title"].ToString();
+                    incidentList.Add(incident);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+                if (reader != null)
+                    reader.Close();
+            }
+            return incidentList;
+        }
+
+    }
+}
