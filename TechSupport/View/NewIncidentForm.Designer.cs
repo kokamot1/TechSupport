62e95512d5220029a00011673f550e7ca6db9db9
Kenji Okamoto <kokamot1@my.westga.edu>
2016-02-05 13:59:31 -0800

    Create a new form for new incidents

---
+++ TechSupport/View/NewIncidentForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TechSupport.View
+{
+    partial class NewIncidentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.customerBox = new System.Windows.Forms.ComboBox();
+            this.productBox = new System.Windows.Forms.ComboBox();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.descriptionBox = new System.Windows.Forms.TextBox();
+            this.createIncidentButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Title:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Description:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // customerBox
+            // 
+            this.customerBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.customerBox.FormattingEnabled = true;
+            this.customerBox.Location = new System.Drawing.Point(134, 29);
+            this.customerBox.Name = "customerBox";
+            this.customerBox.Size = new System.Drawing.Size(248, 21);
+            this.customerBox.TabIndex = 4;
+            // 
+            // productBox
+            // 
+            this.productBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.productBox.FormattingEnabled = true;
+            this.productBox.Location = new System.Drawing.Point(134, 80);
+            this.productBox.Name = "productBox";
+            this.productBox.Size = new System.Drawing.Size(248, 21);
+            this.productBox.TabIndex = 5;
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(134, 129);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(248, 20);
+            this.titleBox.TabIndex = 6;
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.Location = new System.Drawing.Point(134, 179);
+            this.descriptionBox.Multiline = true;
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.Size = new System.Drawing.Size(248, 108);
+            this.descriptionBox.TabIndex = 7;
+            // 
+            // createIncidentButton
+            // 
+            this.createIncidentButton.AutoSize = true;
+            this.createIncidentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createIncidentButton.Location = new System.Drawing.Point(134, 312);
+            this.createIncidentButton.Name = "createIncidentButton";
+            this.createIncidentButton.Size = new System.Drawing.Size(130, 28);
+            this.createIncidentButton.TabIndex = 8;
+            this.createIncidentButton.Text = "Create Incident";
+            this.createIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.AutoSize = true;
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(306, 311);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 28);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // NecIncidentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 375);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.createIncidentButton);
+            this.Controls.Add(this.descriptionBox);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.productBox);
+            this.Controls.Add(this.customerBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NecIncidentForm";
+            this.Text = "NecIncidentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox customerBox;
+        private System.Windows.Forms.ComboBox productBox;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.TextBox descriptionBox;
+        private System.Windows.Forms.Button createIncidentButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
